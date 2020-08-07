3e58a227c1d910308870bc2bdffe195ac8c671fc
AntiqueR <antique.pendulum@gmail.com>
2020-08-07 17:28:39 +0900

    Initial commit

---
+++ AntiquerChain/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using ConsoleAppFramework;
+using Microsoft.Extensions.Hosting;
+
+namespace AntiquerChain
+{
+    class Program : ConsoleAppBase
+    {
+        static async Task Main(string[] args)
+        {
+            await Host.CreateDefaultBuilder().RunConsoleAppFrameworkAsync<Program>(args);
+        }
+    }
+}
