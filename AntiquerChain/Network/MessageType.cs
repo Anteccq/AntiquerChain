8e0fe4408bd489019f0cfe64c07db60c8fbd26ce
AntiqueR <antique.pendulum@gmail.com>
2020-08-10 16:26:27 +0900

    add message structure
    (仮)

---
+++ AntiquerChain/Network/MessageType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntiquerChain.Network
+{
+    public enum MessageType
+    {
+        HandShake,
+        Ping,
+        Inventory,
+        Notice
+    }
+}
