9ae7219a9413edd67419dcbe215aa7e1600b4714
AntiqueR <antique.pendulum@gmail.com>
2020-08-12 23:23:59 +0900

    modify consoleappbase class to AntiquerChain class

---
+++ AntiquerChain/AntiquerChain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleAppFramework;
+using Microsoft.Extensions.Logging;
+
+namespace AntiquerChain
+{
+    public class AntiquerChain : ConsoleAppBase
+    {
+
+    }
+}
