da34e28322ab6c427981cfe868bf8e1f3f8319aa
AntiqueR <antique.pendulum@gmail.com>
2020-08-17 23:13:56 +0900

    add Messenger class and Context

---
+++ AntiquerChain/Network/NetworkContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntiquerChain.Network
+{
+    public static class NetworkContext
+    {
+        public const int SERVER_PORT = 50151;
+        public const int SURFACE_PORT = 50152;
+    }
+}
