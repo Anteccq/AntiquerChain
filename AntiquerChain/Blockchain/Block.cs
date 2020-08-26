07d0c4cba3f19942a7d848b97b023030b12144d9
AntiqueR <antique.pendulum@gmail.com>
2020-08-26 16:56:56 +0900

    add Block class

---
+++ AntiquerChain/Blockchain/Block.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntiquerChain.Blockchain
+{
+    public class Block
+    {
+        public byte[] PreviousBlockHash { get; set; }
+        public byte[] MerkleRootHash { get; set; }
+        public DateTime Timestamp { get; set; }
+        public int Nonce { get; set; }
+        public List<Transaction> Transactions { get; set; }
+    }
+}
