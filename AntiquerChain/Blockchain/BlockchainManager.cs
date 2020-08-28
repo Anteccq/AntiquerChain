d4ab4ea3e9c5617b682921b98a2706ed843c7399
AntiqueR <antique.pendulum@gmail.com>
2020-08-28 18:36:59 +0900

    add Transaction pool and Verify blockchain

--- AntiquerChain/Blockchain/BlockchainManager.cs
+++ AntiquerChain/Blockchain/BlockchainManager.cs
@@ -10,7 +10,21 @@ namespace AntiquerChain.Blockchain
     public class BlockchainManager
     {
         public List<Block> Blockchain { get; } = new List<Block>();
+
         private const int CoinBaseInterval = 20;
+
+        public List<Transaction> TransactionPool { get; } = new List<Transaction>();
+
+        public Transaction[] GetPool() => 
+            TransactionPool.ToArray();
+
+        public void ClearPool(int count) => 
+            TransactionPool.RemoveRange(0, count);
+
+        public void ClearPool(HexString[] strings)
+        {
+            TransactionPool.RemoveAll(x => strings.Contains(x.Id));
+        }
 
         public static Block CreateGenesis()
         {
@@ -50,6 +64,26 @@ namespace AntiquerChain.Blockchain
             var idBytes = JsonSerializer.Serialize(tx);
             tx.Id = new HexString(idBytes);
             return tx;
+        }
+
+        public bool VerifyBlockchain()
+        {
+            /*var i = 0;
+            while (i < Blockchain.Count)
+            {
+                var rearData = JsonSerializer.Serialize(Blockchain[i]);
+                var prevHash = Blockchain[i + 1].PreviousBlockHash.Bytes;
+                if (prevHash != HashUtil.DoubleSHA256(rearData)) return false;
+                i++;
+            }*/
+
+            var isRight = Blockchain.Take(Blockchain.Count - 1).SkipWhile((block, i) =>
+            {
+                var leadData = JsonSerializer.Serialize(block);
+                return Blockchain[i + 1].PreviousBlockHash.Bytes != leadData;
+            }).Any();
+
+            return !isRight;
         }
 
         public static int GetSubsidy(int height) =>
